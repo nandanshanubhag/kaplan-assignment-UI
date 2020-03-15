12d105b9ec909e30544d0c3092af0c8a4f2712e1
Nandan Shanubhag <nandan@techjini.com>
2020-03-15 19:31:16 +0530

    created service method to get upcoming lessons

--- src/app/modules/lessons/pages/upcoming-lessons/upcoming-lessons.component.ts
+++ src/app/modules/lessons/pages/upcoming-lessons/upcoming-lessons.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+// Services
+import { LessonsService } from '../../lessons.service';
+
 @Component({
-  selector: 'app-upcoming-lessons',
-  templateUrl: './upcoming-lessons.component.html',
-  styleUrls: ['./upcoming-lessons.component.scss']
+    selector: 'app-upcoming-lessons',
+    templateUrl: './upcoming-lessons.component.html',
+    styleUrls: ['./upcoming-lessons.component.scss']
 })
 export class UpcomingLessonsComponent implements OnInit {
+    constructor(private lessonsService: LessonsService) {}
 
-  constructor() { }
+    public ngOnInit() {
+        this.getUpcomingLessons();
+    }
 
-  ngOnInit() {
-  }
-
+    private getUpcomingLessons() {
+        this.lessonsService.getUpcomingLessons().subscribe(
+            res => {
+                console.log('upcoming lessons', res);
+            },
+            err => {
+                console.log('Error while getting upcoming lessons', err.message);
+            }
+        );
+    }
 }
